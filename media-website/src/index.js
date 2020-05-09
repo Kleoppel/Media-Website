d7d8d1b821c6b5ee68f7300560737f64db4a0818
Jake Kanak <jakekanak@gmail.com>
2020-05-09 08:55:12 -0500

    Inital shot --- I cant get it to read the display component but I am out of time

---
+++ media-website/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import Display from './components/Display';
+import Callback from './components/Callback';
+import { Router, Route, BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+
+const Root = () => {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Route path="/" component={Display}/>
+        <Route path="/callback" component={Callback} />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Upload from './components/Upload';
+// import Display from './components/Display';
+// import Callback from './components/Callback';
+// import registerServiceWorker from './registerServiceWorker';
+// import { Router, Route, browserHistory } from 'react-router';
+// import { requireAuth } from './utils/AuthService';
+
+// const Root = () => {
+//   return (
+//     <div className="container">
+//       <Router history={browserHistory}>
+//         <Route path="/" component={Display}/>
+//         <Route path="/upload" component={Upload} onEnter={requireAuth} />
+//         <Route path="/callback" component={Callback} />
+//       </Router>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(<Root />, document.getElementById('root'));
+// registerServiceWorker();
