3a95debec498c3cf22c6ce461862f7b45ca61aa2
Terry Kleoppel <terrykleoppel@gmail.com>
2020-05-31 17:18:03 -0500

    accidentally recommitted footer style. Removing.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function Footer(){
     return (
-        <div 
-            className="Footer"
-            style={{
-                'backgroundColor': 'black',
-                'color': "white",
-                'position': 'absolute',
-                'left': '0',
-                'bottom':'0',
-                'right':'0'
-            }}
+        <div
+            // className="Footer"
+            // style={{
+            //     'backgroundColor': 'black',
+            //     'color': "white",
+            //     'position': 'absolute',
+            //     'left': '0',
+            //     'bottom':'0',
+            //     'right':'0'
+            // }}
         >
             <h1>Footer</h1>
         </div>
